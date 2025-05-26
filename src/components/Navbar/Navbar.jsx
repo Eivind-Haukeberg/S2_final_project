f1d804b05812f98145c550440efd60b92e2e6dec
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-05-26 16:25:55 +0200

    add logo - add variables.css - add navbar.css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import logo from '../../assets/logo/OneApp_logo.svg';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className='navigation-bar'>
-      <div className='navigation-bar__logo'>OneApp</div>
+      <div className='navigation-bar__logo'>
+        <img src={logo} alt='OneApp Logo' className='logo-image' />
+      </div>
       <div className='navigation-bar__links'>
         <Link to='/' className='navigation-bar__link'>
           Home
