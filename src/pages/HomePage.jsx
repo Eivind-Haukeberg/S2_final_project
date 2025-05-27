bc96a3f444c1a9093760fe540a8e5c4ec2f375d5
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-05-27 19:49:31 +0200

    add login and register to home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,14 @@
+import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
+
 function HomePage() {
-  return <div className='home-page'>Welcome to OneApp</div>;
+  return (
+    <div className='home-page'>
+      <h1>Welcome to OneApp</h1>
+      <LoginForm />
+      <RegisterForm />
+    </div>
+  );
 }
+
 export default HomePage;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,4 +1,45 @@
+import { useEffect, useState } from 'react';
+import { auth, db } from '../services/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
+
 function MyPage() {
-  return <div className='my-page'>User info and list will appear here</div>;
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = auth.currentUser;
+
+      if (user) {
+        const userDoc = await getDoc(doc(db, 'users', user.uid));
+        if (userDoc.exists()) {
+          setUserInfo(userDoc.data());
+        }
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <div className='my-page'>
+      <h2 className='my-page__title'>My Information</h2>
+      {userInfo ? (
+        <div className='my-page__info'>
+          <p>
+            <strong>Name:</strong> {userInfo.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {userInfo.email}
+          </p>
+          <p>
+            <strong>Country:</strong> {userInfo.country}
+          </p>
+        </div>
+      ) : (
+        <p>Loading user info...</p>
+      )}
+    </div>
+  );
 }
+
 export default MyPage;
