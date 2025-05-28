96c88f8e0dbaefa353b6df9a8454fc3b9bb71f77
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-05-28 15:48:45 +0200

    add MyList to MyPage.jsx

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { auth, db } from '../services/firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
+import MyListGrid from '../components/MyListGrid/MyListGrid';
+import '../styles/MyPage.css';
 
 function MyPage() {
   const [userInfo, setUserInfo] = useState(null);
@@ -8,7 +10,6 @@ function MyPage() {
   useEffect(() => {
     const fetchUser = async () => {
       const user = auth.currentUser;
-
       if (user) {
         const userDoc = await getDoc(doc(db, 'users', user.uid));
         if (userDoc.exists()) {
@@ -24,17 +25,20 @@ function MyPage() {
     <div className='my-page'>
       <h2 className='my-page__title'>My Information</h2>
       {userInfo ? (
-        <div className='my-page__info'>
-          <p>
-            <strong>Name:</strong> {userInfo.name}
-          </p>
-          <p>
-            <strong>Email:</strong> {userInfo.email}
-          </p>
-          <p>
-            <strong>Country:</strong> {userInfo.country}
-          </p>
-        </div>
+        <>
+          <div className='my-page__info'>
+            <p>
+              <strong>Name:</strong> {userInfo.name}
+            </p>
+            <p>
+              <strong>Email:</strong> {userInfo.email}
+            </p>
+            <p>
+              <strong>Country:</strong> {userInfo.country}
+            </p>
+          </div>
+          <MyListGrid />
+        </>
       ) : (
         <p>Loading user info...</p>
       )}
