42e8af525e14d7a4c406016103461d6a0c3ed1d1
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-06-01 19:31:58 +0200

    add responsive styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,45 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo/OneApp_logo.svg';
 import styles from './Navbar.module.css';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className={styles['navigation-bar']}>
-      <div className={styles['navigation-bar__logo']}>
-        <Link to='/'>
+      <div className={styles['navigation-bar__container']}>
+        <Link to='/' className={styles['navigation-bar__logo']}>
           <img
             src={logo}
             alt='OneApp Logo'
             className={styles['navigation-bar__logo-image']}
           />
         </Link>
-      </div>
-      <div className={styles['navigation-bar__links']}>
-        {/* <Link to='/' className={styles['navigation-bar__link']}>
-          Home
-        </Link> */}
-        <Link to='/my-page' className={styles['navigation-bar__link']}>
-          My Page
-        </Link>
-        <Link to='/admin' className={styles['navigation-bar__link']}>
-          Admin
-        </Link>
+
+        <button
+          className={styles['navigation-bar__burger']}
+          onClick={() => setIsMenuOpen((prev) => !prev)}>
+          ☰
+        </button>
+
+        <div
+          className={`${styles['navigation-bar__links']} ${
+            isMenuOpen ? styles['navigation-bar__links--open'] : ''
+          }`}>
+          <Link
+            to='/my-page'
+            className={styles['navigation-bar__link']}
+            onClick={() => setIsMenuOpen(false)}>
+            My Page
+          </Link>
+          <Link
+            to='/admin'
+            className={styles['navigation-bar__link']}
+            onClick={() => setIsMenuOpen(false)}>
+            Admin
+          </Link>
+        </div>
       </div>
     </nav>
   );
